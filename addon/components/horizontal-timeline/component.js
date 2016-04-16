876e80b8174f066db3c95eb791b3ee720f6f2214
Robert Wagner <rwwagner@vt.edu>
2016-04-16 09:53:01 -0400

    Breaks labels and lines into functions

--- addon/components/horizontal-timeline/component.js
+++ addon/components/horizontal-timeline/component.js
@@ -11,7 +11,7 @@ export default Component.extend({
     this.element.width = document.documentElement.clientWidth - 100;
     this.drawLanes();
   },
-  drawLanes(){
+  drawLanes() {
     const ctx = this.element.getContext('2d');
 
     ctx.strokeStyle = "#000000";
@@ -19,24 +19,29 @@ export default Component.extend({
 
     ctx.beginPath();
 
-    var x = null;
     const yearSpan = this.get('endYear') - this.get('startYear');
     const laneSize = Math.floor(this.element.width / (yearSpan / 50));
     const numLanes = Math.floor(this.element.width / laneSize);
 
     for (let i = 1; i <= numLanes; i++) {
-      x = (i * laneSize);
-      ctx.moveTo(x, 0);
-      ctx.lineTo(x, this.element.height - 30);
-      ctx.stroke();
-      ctx.fillStyle = "#000000";
-      ctx.font = "normal normal 14px Helvetica";
-      ctx.textBaseline = "middle"; // how to align the text vertically
-      ctx.textAlign = "center"; // how to align the text horizontally
-      const label = String(this.get('startYear') + i * 50);
-      ctx.fillText(label, x, this.element.height - 10); // text, x, y
+      const x = (i * laneSize);
+      this.drawLines(ctx, x);
+      this.labelLines(ctx, i, x);
     }
 
     ctx.closePath();
+  },
+  drawLines(ctx, x){
+    ctx.moveTo(x, 0);
+    ctx.lineTo(x, this.element.height - 30);
+    ctx.stroke();
+  },
+  labelLines(ctx, i, x){
+    ctx.fillStyle = "#000000";
+    ctx.font = "normal normal 14px Helvetica";
+    ctx.textBaseline = "middle";
+    ctx.textAlign = "center";
+    const label = String(this.get('startYear') + i * 50);
+    ctx.fillText(label, x, this.element.height - 10);
   }
 });
