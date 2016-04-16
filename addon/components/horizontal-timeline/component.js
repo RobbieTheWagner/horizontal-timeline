c93283feb0309f0d11f84d3cda110990ea75a67a
Robert Wagner <rwwagner@vt.edu>
2016-04-16 12:57:10 -0400

    Make rectangles appear on new lines

--- addon/components/horizontal-timeline/component.js
+++ addon/components/horizontal-timeline/component.js
@@ -1,10 +1,26 @@
 import Ember from 'ember';
-const {$, Component, computed} = Ember;
+const {$, Component, computed, Object} = Ember;
+
+const Rectangle = Object.extend({
+  x: null,
+  y: null,
+  height: null,
+  width: null,
+  intersects(rect) {
+    if (rect.x < this.x + this.width && this.x < rect.x + rect.width && rect.y < this.y + this.height) {
+      return this.y < rect.y + rect.height;
+    }
+    else {
+      return false;
+    }
+  }
+});
 
 export default Component.extend({
   tagName: 'canvas',
   classNames: ['timeline'],
   endYear: 2016,
+  rectangles: [],
   xOffset: 15,
   pixelWidthForYear: computed('model', function() {
     return document.documentElement.clientWidth / this.get('yearSpan');
@@ -56,15 +72,33 @@ export default Component.extend({
   drawRectangles() {
     const ctx = this.get('ctx');
     const people = this.get('model');
+    this.set('rectangleHeight', this.element.height / 50);
     people.forEach((person) => {
-      const startX = (person.birth - this.get('startYear')) * this.get('pixelWidthForYear') + this.get('xOffset');
-      const startY = 0;
+      const x = (person.birth - this.get('startYear')) * this.get('pixelWidthForYear') + this.get('xOffset');
       const death = person.death !== Infinity ? person.death : 2016;
       const lifespan = death - person.birth;
       const width = lifespan * this.get('pixelWidthForYear');
-      const height = 100;
-      ctx.rect(startX, startY, width, height);
+      const newRectangle = Rectangle.create({
+        x,
+        y: 0,
+        height: this.get('rectangleHeight'),
+        width
+      });
+      this.checkForIntersection(newRectangle);
+      this.get('rectangles').push(newRectangle);
+    });
+    this.get('rectangles').forEach((rect) => {
+      ctx.rect(rect.x, rect.y, rect.width, rect.height);
       ctx.stroke();
+    })
+  },
+  checkForIntersection(newRectangle) {
+    this.get('rectangles').forEach((rect) => {
+      if (newRectangle.intersects(rect)) {
+        newRectangle.set('y', rect.y + this.get('rectangleHeight'));
+        return newRectangle;
+      }
     });
   }
-});
+})
+;
